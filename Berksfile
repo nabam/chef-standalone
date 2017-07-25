24280256676543fbbd4b9165df30f8b2465fad04
Lev Popov <leo@nabam.net>
2017-07-25 22:32:13 +0300

    systemd units

--- Berksfile
+++ Berksfile
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
-cookbook 'apt', '~> 6.1.3'
 
 metadata
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,16 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+include_recipe "systemd::timezone"
+
+user 'media' do
+  uid '1001'
+  gid '1001'
+  home '/home/media'
+  shell '/bin/false'
+  manage_home :create
+end
+
 ["docker.io", "sysdig", "sysdig-dkms", "bash-completion",
  "linux-headers-generic", "git", "apache2-utils", "tcpdump",
  "htop", "transmission-remote-cli", "tcpdump", "dnsutils",
@@ -11,3 +21,25 @@
   package pkg
 end
 
+["docker-nginx.service", "docker-openvpn@.service",
+ "docker-plex.service", "docker-transmission.service"].each do |unit|
+  template "/etc/systemd/system/#{unit}" do
+    source "etc/systemd/system/#{unit}.erb"
+    variables({
+      timezone: node["systemd"]["timezone"],
+      hostname: node["hostname"],
+      url: node["nginx"]["url"],
+      email: node["nginx"]["email"],
+      uid: 1001,
+      gid: 1001,
+      plex_claim: node["plex"]["claim"]
+    })
+  end
+end
+
+["docker-nginx.service", "docker-openvpn@#{node["openvpn"]["fqdn"]}.service",
+ "docker-plex.service", "docker-transmission.service"].each do |unit|
+  service unit do
+    action [:enable, :start]
+  end
+end
