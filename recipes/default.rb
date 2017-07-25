18aa6988f44de10b7c375e5120664e0ab21cce20
Lev Popov <leo@nabam.net>
2017-07-25 21:16:20 +0300

    install packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+["docker.io", "sysdig", "sysdig-dkms", "bash-completion",
+ "linux-headers-generic", "git", "apache2-utils", "tcpdump",
+ "htop", "transmission-remote-cli", "tcpdump", "dnsutils",
+ "silversearcher-ag", "tree", "file"].each do |pkg|
+  package pkg
+end
+
