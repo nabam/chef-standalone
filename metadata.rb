e21729aaa0da7d515ff657abe8a0ea8b23313dc8
Lev Popov <leo@nabam.net>
2017-07-25 23:47:59 +0300

    docker volumes

--- metadata.rb
+++ metadata.rb
@@ -8,3 +8,4 @@ version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 depends "systemd"
+depends "docker"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,12 +6,15 @@
 
 include_recipe "systemd::timezone"
 
+execute "systemctl daemon-reload" do
+  action :nothing
+end
+
 user 'media' do
-  uid '1001'
-  gid '1001'
-  home '/home/media'
+  uid   1001
+  gid   1001
+  home  '/home/media'
   shell '/bin/false'
-  manage_home :create
 end
 
 ["docker.io", "sysdig", "sysdig-dkms", "bash-completion",
@@ -21,19 +24,26 @@ end
   package pkg
 end
 
+["nginx-config", "ovpn-data-#{node["openvpn"]["fqdn"]}",
+ "plex-config", "plex-transcode", "transmission-config"].each do |volume|
+  docker_volume volume
+end
+
 ["docker-nginx.service", "docker-openvpn@.service",
  "docker-plex.service", "docker-transmission.service"].each do |unit|
   template "/etc/systemd/system/#{unit}" do
     source "etc/systemd/system/#{unit}.erb"
     variables({
-      timezone: node["systemd"]["timezone"],
-      hostname: node["hostname"],
-      url: node["nginx"]["url"],
-      email: node["nginx"]["email"],
-      uid: 1001,
-      gid: 1001,
+      timezone:   node["systemd"]["timezone"],
+      fqdn:       node["fqdn"],
+      hostname:   node["hostname"],
+      url:        node["nginx"]["url"],
+      email:      node["nginx"]["email"],
+      uid:        1001,
+      gid:        1001,
       plex_claim: node["plex"]["claim"]
     })
+    notifies :run, 'execute[systemctl daemon-reload]'
   end
 end
 
